d03833522d2c185abad27c85fa2d42367af38309
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2021-11-20 09:39:37 -0300

    atualizar

--- Oficina/ProjetoOficina.java/src/Cliente.java
+++ Oficina/ProjetoOficina.java/src/Cliente.java
@@ -1,12 +1,50 @@
-
+// Atribubuto e Metodos
 
 public class Cliente {
 
-    public String elevadores;
-    public String peças;
-    public int mecanicos;
-    public boolean aberto;
-    public void status() {
+    public String peças;//visibilidade publica
+    public String elevadores;//visibilidade publica
+    public int mecanicos;//visibilidade publica
+    public boolean aberto = true;//visibilidade publica
+
+    public void abrir(){
+        this.aberto = true;
     }
 
+    public void fechado(){
+        this.aberto = false;
+
+    }
+
+    public void consertarCarros(){
+        if(this.aberto == true){
+            System.out.println("Podemos consertar os carros! ");
+        } else{ 
+            System.out.println("Não podemos trabalhar! ");
+        }
+
+    }
+
+    public void orcamentoDeManutencao(){
+
+    }
+
+    public void trocaDePneus(){
+
+    }
+
+    public void trocaDeSuspenacao(){
+
+    }
+
+    public void status(){
+        System.out.println("A oficina ta aberta? " + this.aberto);
+        System.out.println("Todas as peças estao prontas para troca? " + this.peças);
+        System.out.println("Quantos mecanicos vao trabalhar hoje? " + this.mecanicos);
+        System.out.println("Esta tudo certo com os elevadores? " + this.elevadores);
+    }
+
+    public void aberto() {
+       
+    }
 }
--- Oficina/ProjetoOficina.java/src/Conserto.java
+++ Oficina/ProjetoOficina.java/src/Conserto.java
@@ -1,12 +1,13 @@
+//Instância
 public class Conserto {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Cliente c1 = new Cliente();
         c1.peças = "ok";
         c1.elevadores = "funcionando";
         c1.mecanicos = 2;
-        c1.aberto = true;
+        c1.aberto();
         c1.status();
-        
-
+        c1.consertarCarros();
+        //O Aqui é instanciado todos os atributos,ou seja, é criado o Objeto
     }
 }
